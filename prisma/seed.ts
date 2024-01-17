4a27166da4af99d6f76d5f6a30cc71532d301f4b
Ruiz, Jose <Jose.Ruiz@jll.com>
2024-01-18 00:12:57 +0100

    do prisma seed previous to tests

---
+++ prisma/seed.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // Seed the User table
+  const user = await prisma.user.create({
+    data: {
+      email: "user@example.com",
+      name: "John Doe",
+      image: "http://example.com/image.jpg",
+      role: "USER", // or 'ADMIN'
+      // Add other fields as necessary
+    },
+  });
+
+  console.log(`User created: ${user.email}`);
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
