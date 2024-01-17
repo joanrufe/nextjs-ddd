8a378d47cc79e2f5ee43b2705389bae252cbcfe0
Ruiz, Jose <Jose.Ruiz@jll.com>
2024-01-17 22:56:22 +0100

    starting with e2e testing

---
+++ e2e/example.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+
+test("homepage is accessible", async ({ page }) => {
+  // Navigate to the local application
+  await page.goto("http://localhost:3000/");
+
+  // Check if the page is loaded by looking for a specific element or text
+  // This is an example, replace 'Some text or element' with something you expect on your page
+  expect(page).toBeDefined();
+});
+
+test("specific element is present", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+
+  // Replace 'ElementSelector' with the actual selector of an element you expect to find
+  await expect(
+    page.locator('a[href="https://github.com/joanrufe/nextjs-ddd"]')
+  ).toBeVisible();
+});
